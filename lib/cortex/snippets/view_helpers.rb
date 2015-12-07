49687f405405d0ab6dbef20ad6c6bc2d128f70f6
Alex Tharp <toastercup@gmail.com>
2015-12-07 09:45:02 -0600

    Move client to a singleton pattern. Makes it easier to work with ViewHelpers and we shouldn't be encouraging multiple instances, anyways.

--- lib/cortex/snippets/view_helpers.rb
+++ lib/cortex/snippets/view_helpers.rb
@@ -2,39 +2,39 @@ module Cortex
   module Snippets
     module ViewHelpers
       def snippet(id)
-        content_tag(:snippet, current_webpage[:snippets].find { |snippet| snippet.name == id }, id: id)
+        content_tag(:snippet, Client::current_webpage[:snippets].find { |snippet| snippet.name == id }, id: id)
       end
 
       def seo_title
-        current_webpage[:seo_title]
+        Client::current_webpage[:seo_title]
       end
 
       def seo_description
-        current_webpage[:seo_description]
+        Client::current_webpage[:seo_description]
       end
 
       def noindex
-        current_webpage[:noindex]
+        Client::current_webpage[:noindex]
       end
 
       def nofollow
-        current_webpage[:nofollow]
+        Client::current_webpage[:nofollow]
       end
 
       def noodp
-        current_webpage[:noodp]
+        Client::current_webpage[:noodp]
       end
 
       def nosnippet
-        current_webpage[:nosnippet]
+        Client::current_webpage[:nosnippet]
       end
 
       def noarchive
-        current_webpage[:noarchive]
+        Client::current_webpage[:noarchive]
       end
 
       def noimageindex
-        current_webpage[:noimageindex]
+        Client::current_webpage[:noimageindex]
       end
     end
   end
