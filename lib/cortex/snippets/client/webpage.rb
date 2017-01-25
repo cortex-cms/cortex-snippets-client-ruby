0425420536814744536a4f7ec9f88e2d7db5640f
ElliottAYoung <elliott.a.young@gmail.com>
2017-01-25 16:33:09 -0600

    Refactor webpage class

--- lib/cortex/snippets/client/webpage.rb
+++ lib/cortex/snippets/client/webpage.rb
@@ -1,26 +1,21 @@
 module Cortex
-  module Snippet
+  module Snippets
     module Client
       class Webpage
-        attr_reader :webpage
-
-        def initialize(request, cortex_client)
-          sanitized_url = sanitized_webpage_url(request.original_url)
-          @webpage = Rails.cache.fetch("webpages/#{cortex_client.access_token.client.id}/#{sanitized_url}", race_condition_ttl: 10) do
-            cortex_client.webpages.get_feed(sanitized_url).contents
-          end
+        def initialize(cortex_client, url)
+          @webpage = cortex_client.webpages.get_feed(url).contents
         end
 
         def seo_title
-          webpage[:seo_title]
+          @webpage[:seo_title]
         end
 
         def seo_description
-          webpage[:seo_description]
+          @webpage[:seo_description]
         end
 
         def seo_keywords
-          webpage[:seo_keyword_list]
+          @webpage[:seo_keyword_list]
         end
 
         def seo_robots
@@ -28,49 +23,41 @@ module Cortex
           index_options = [:noindex, :nofollow, :noodp, :nosnippet, :noarchive, :noimageindex]
 
           index_options.each do |index_option|
-            robot_information << index_option if webpage[index_option]
+            robot_information << index_option if @webpage[index_option]
           end
 
           robot_information
         end
 
         def noindex
-          webpage[:noindex]
+          @webpage[:noindex]
         end
 
         def nofollow
-          webpage[:nofollow]
+          @webpage[:nofollow]
         end
 
         def noodp
-          webpage[:noodp]
+          @webpage[:noodp]
         end
 
         def nosnippet
-          webpage[:nosnippet]
+          @webpage[:nosnippet]
         end
 
         def noarchive
-          webpage[:noarchive]
+          @webpage[:noarchive]
         end
 
         def noimageindex
-          webpage[:noimageindex]
+          @webpage[:noimageindex]
         end
 
         def dynamic_yield
           {
-            sku: webpage[:dynamic_yield_sku],
-            category: webpage[:dynamic_yield_category]
+            sku: @webpage[:dynamic_yield_sku],
+            category: @webpage[:dynamic_yield_category]
           }
-        end
-
-        private
-
-        def sanitized_webpage_url(url)
-          uri = Addressable::URI.parse(url)
-          path = uri.path == '/' ? uri.path : uri.path.chomp('/')
-          "#{uri.scheme}://#{uri.authority}#{path}"
         end
       end
     end
